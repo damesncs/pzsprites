39519f9b6b83d3aa382a9985ac151b39842db23d
David Ames <david_ames@norfolkchristian.org>
2026-04-27 13:20:40 -0400

    camera scale by speed and other tweaks

--- demos/plane/plane.js
+++ demos/plane/plane.js
@@ -33,12 +33,14 @@ const
     COL_LIMIT = 2.5, // hard limit on CoL (to reduce craziness)
 
     // plane parameters
-    MAX_THRUST = 3, // i.e., engine power
-    THRUST_INCR = 0.5, // thrust to add each frame while engine on
+    MAX_THRUST = 2, // i.e., engine power
+    THRUST_INCR = 0.1, // thrust to add each frame while engine on
     COD = 0.027, // coefficient of drag - for Cessna 172
     FRONT_AREA = 3, // frontal area for drag, sq meters
     WING_PLAN_AREA = 16.5, // wing area for lift, sq meters
-    ELEVATOR_AREA = 1,
+    ELEVATOR_INCR = 0.1, // elevator increment each frame key is pressed
+    MAX_ELEVATOR = 1,
+    ELEVATOR_AREA = 0.9,
 
     // world parameters
     GROUND_HEIGHT = 30,
@@ -49,8 +51,8 @@ const
     ;
 
 // terrain generation parameters
-const GROUND_SEGMENTS = 200;
-const MAX_VERTICAL_CHANGE = 3;
+const GROUND_SEGMENTS = 2000;
+const MAX_VERTICAL_CHANGE = 2;
 
 // sprites
 let plane, 
@@ -66,21 +68,22 @@ let plane,
     
     leftBound, rightBound, planeCameraMargin;
 
-let cameraScale = 10;
+let maxCameraScale = 9;
+let currentCameraScale = maxCameraScale;
 
 // physics variables
 let 
-    speed = {x:0, y:0},
+    pos = {x:0, y:0},    
+    linearSpeed = 0,
     dragVector = {x:0, y:0},
-    dragArea = 0,
     aoa = 0,
     col = 0,
     lift = 0,
     liftVector = {x:0, y:0},
-    xForce = 0,
     thrust = 0,
     thrustVector = {x:0, y:0},
     elevForceVector = {x:0, y:0},
+    elevatorAmount = 0,
     elevatorState = "-",
     flowVector = {x: 0, y: 0},
     noseAngle = 0,
@@ -95,9 +98,9 @@ let world;
 
  async function start() {
     world = setupWorld("canvas", 800, 500);
-    world.setWorldDimensions(10000, 500);
+    world.setWorldDimensions(100000, 500);
     world.setGravity({ x: 0, y: 9.8 });
-    world.setCameraScale(cameraScale);
+    world.setCameraScale(maxCameraScale);
 
     let eachSegmentLength = world.getWidth() / GROUND_SEGMENTS;                                              
     let vertices = [{ x: 0, y: world.getHeight() / 2 }];
@@ -165,9 +168,16 @@ let world;
 }
 
 function drawEachFrame(timestamp){
-    let planePos = plane.getPosition();
-    world.setCameraPosition(planePos.x, planePos.y);
-    world.setCameraScale(cameraScale);
+    const planePos = plane.getPosition(); // Tricky!! this is a reference to a Vec22 object whose values will change
+    pos = { x: planePos.x, y: planePos.y };
+    linearSpeed =  getLinearSpeedFromVector(plane.getLinearVelocity()); // at CoG
+
+    world.setCameraPosition(pos.x, pos.y);
+    currentCameraScale = maxCameraScale / linearSpeed ** 0.1;
+    if(currentCameraScale < 1 || currentCameraScale > maxCameraScale)
+        currentCameraScale = maxCameraScale;
+    world.setCameraScale(currentCameraScale);
+
     calculatePlaneForces();
     
     renderFrame();
@@ -178,6 +188,9 @@ function drawEachFrame(timestamp){
 
 function drawPhysicsVars(){
     const vars = [
+        `CamScale: ${currentCameraScale} \n`,
+        `Position: ${pos.x.toFixed(2)}, ${pos.y.toFixed(2)}\n`,
+        `LinearSpeed: ${linearSpeed.toFixed(2)}\n`,
         `Thrust: ${thrust.toFixed(2)}\n`,
         `ThrustV: ${thrustVector.x.toFixed(2)}, ${thrustVector.y.toFixed(2)}\n`,
         `FlowV: ${flowVector.x.toFixed(2)}, ${flowVector.y.toFixed(2)}\n`,
@@ -186,10 +199,9 @@ function drawPhysicsVars(){
         `CoL: ${col.toFixed(2)}\n`,
         `Lift: ${lift.toFixed(2)}\n`,
         `LiftV: ${liftVector.x.toFixed(2)}, ${liftVector.y.toFixed(2)}\n`,
-        `PlaneV: ${speed.x.toFixed(2)}, ${speed.y.toFixed(2)}\n`,
         `DragV: ${dragVector.x.toFixed(2)}, ${dragVector.y.toFixed(2)}\n`,
-        `Elev: ${elevatorState} \n`,
-        `ElevV: ${elevForceVector.x.toFixed(2)}, ${elevForceVector.y.toFixed(2)}\n`,
+        `Elev: ${elevatorState} ${elevatorAmount.toFixed(2)} \n`,
+        `ElevV: ${elevForceVector.x.toFixed(2)}, ${elevForceVector.y.toFixed(2)}\n`
     ];
     const textSize = 16;
     vars.forEach((t, i) => {
@@ -199,12 +211,8 @@ function drawPhysicsVars(){
 }
 
 function calculatePlaneForces(){
-    const planePos = plane.getPosition(); // Tricky!! this is a reference to a Vec22 object whose values will change
-
-    speed = plane.getLinearVelocity(); // at CoG
-    
     // get flow angle relative to plane CoG - averaged over n frames
-    vectorRefPoints.push({ x: planePos.x, y: planePos.y });
+    vectorRefPoints.push({ x: pos.x, y: pos.y });
     if (vectorRefPoints.length === VECTOR_REF_FRAMES){
         vectorRefPoints.shift();
         vectorRefAvgPoint = vectorRefPoints.reduce((acc, cur, i) => {
@@ -216,14 +224,14 @@ function calculatePlaneForces(){
             
     debugRefPoint.setPosition(vectorRefAvgPoint);
 
-    flowVector = getVector(planePos, vectorRefAvgPoint);
+    flowVector = getVector(pos, vectorRefAvgPoint);
 
     let flowAngle = plane.angleTo({ x: vectorRefAvgPoint.x, y: vectorRefAvgPoint.y });
     noseAngle = plane.angleTo(planeNose);
     aoa = (flowAngle - noseAngle);
 
     col = getCoL(aoa, "cubic2", COL_LIMIT);
-    lift = getLift(flowVector.x, col); // TODO speed needs to be speed along flow vector??
+    lift = getLift(getLinearSpeedFromVector(flowVector), col);
 
     // apply lift perpendicularly to air flow
     liftVector.x = flowVector.y * lift;
@@ -237,26 +245,30 @@ function calculatePlaneForces(){
     plane.applyForceToCenter(thrustVector);
     
     // apply drag opposite air flow
-    const oppositeFlowVector = getVector(vectorRefAvgPoint, planePos);
+    const oppositeFlowVector = getVector(vectorRefAvgPoint, pos);
     // not sure this makes sense
-    const dragForce = getDrag(getLinearVelocity(oppositeFlowVector));
+    const dragForce = getDrag(getLinearSpeedFromVector(oppositeFlowVector));
     dragVector.x = oppositeFlowVector.x * dragForce;
     dragVector.y = oppositeFlowVector.y * dragForce;
     plane.applyForceToCenter(dragVector);
 
     // control forces - elevator
+
     // apply force perpendicular to flow 
-    const elevForce = ELEVATOR_AREA * (getLinearVelocity(flowVector) ** 2) * SPEED_FACTOR;
+    const elevForce = ELEVATOR_AREA * (getLinearSpeedFromVector(flowVector) ** 2) * SPEED_FACTOR * elevatorAmount;
     if(elevatorState === 'DN'){ // i.e., stick forward
-        
-        elevForceVector = { x: elevForce * oppositeFlowVector.y, y: elevForce * oppositeFlowVector.x};
+        // apply force from below plane (perpendicular to flow)
+        elevForceVector = { x: elevForce * flowVector.x, y: -(elevForce * flowVector.y) };
+        if(elevatorAmount < MAX_ELEVATOR) elevatorAmount += ELEVATOR_INCR;
     } else if(elevatorState === 'UP'){ // i.e., stick back
-        
-        elevForceVector = { x: elevForce * oppositeFlowVector.y, y: -(elevForce * oppositeFlowVector.x)};
+        // apply force from above plane (perpendicular to flow)
+        elevForceVector = { x: elevForce * flowVector.x, y: elevForce * flowVector.y };
+        if(elevatorAmount < MAX_ELEVATOR) elevatorAmount += ELEVATOR_INCR;
     } else {
+        elevatorAmount = 0;
         elevForceVector = { x: 0, y: 0 };
     }
-    // planeTail.applyForceToCenter(elevForceVector);
+    planeTail.applyForceToCenter(elevForceVector);
 }
 
 function getDrag(speed){
@@ -302,7 +314,8 @@ function addVectors(v1, v2){
     return {x: v1.x + v2.x, y: v1.y + v2.y };
 }
 
-function getLinearVelocity(v){
+
+function getLinearSpeedFromVector(v){
     return Math.abs(v.x) + Math.abs(v.y);
 }
 
